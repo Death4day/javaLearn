9947a1ee63fd958e63e92af24c32d474a397c00d
Death4day <mikoto1337228@gmail.com>
2020-04-03 17:08:35 +0700

    Нужное

---
+++ src/src/Method.java
@@ -0,0 +1,13 @@
+public class Method {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printSeveralTimes(String текстДляВывода,int количествоСтрок) {
+        for (int i=0;i<количествоСтрок;i++)
+        {
+            System.out.println(текстДляВывода);
+        }
+    }
+
+}
---
+++ src/src/javarush/level01/lection04/Cycle.java
@@ -0,0 +1,9 @@
+package javarush.level01.lection04;
+
+public class Cycle {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i = i + 1) {
+            System.out.println("JGFBE");
+        }
+    }
+}
